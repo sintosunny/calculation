6126ccb6e0b9e1fb140b79c540fc16787ff96016
sintosunny <sintosunny2002@gamil.com>
2024-10-08 10:46:19 +0530

    multiplication

---
+++ mul.py
@@ -0,0 +1,10 @@
+# Function to multiply two numbers
+def multiply_numbers(a, b):
+    return a * b
+
+# Example usage
+num1 = 10
+num2 = 5
+
+result = multiply_numbers(num1, num2)
+print(f"The multiplication of {num1} and {num2} is: {result}")

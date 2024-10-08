8b647ba2ba21cedcfd531639925189181036dabf
sintosunny <sintosunny2002@gamil.com>
2024-10-08 12:07:30 +0530

    sumv2

--- sum.py
+++ sum.py
@@ -4,8 +4,8 @@ def add_numbers(a, b):
     return a + b
 
 # Example usage
-num1 = 10
+num4 = 18
 num2 = 5
 
 result = add_numbers(num1, num2)
-print(f"The sum of {num1} and {num2} is: {result}")
+print(f"The sum of {num4} and {num2} is: {result}")

e86928934acf111d53d72536caedf801a655c44d
sintosunny <sintosunny2002@gamil.com>
2024-10-09 10:37:54 +0530

    sqrootv1

---
+++ sqroot.py
@@ -0,0 +1,10 @@
+import math
+
+# Input number from the user
+num = float(input("Enter a number: "))
+
+# Calculate the square root
+sqrt = math.sqrt(num)
+
+# Display the result
+print(f"The square root of {num} is {sqrt}")
